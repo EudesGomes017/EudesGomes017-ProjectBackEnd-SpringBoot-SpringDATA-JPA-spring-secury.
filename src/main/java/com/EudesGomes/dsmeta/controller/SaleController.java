0d45ab379593d5bf10b6ec045d47e5ff09ca11bd
EudesGomes017 <eudesgomes017@gmail.com>
2022-09-10 19:44:47 -0300

    Twilio SMS

--- src/main/java/com/EudesGomes/dsmeta/controller/SaleController.java
+++ src/main/java/com/EudesGomes/dsmeta/controller/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.EudesGomes.dsmeta.entities.Sale;
 import com.EudesGomes.dsmeta.service.SaleService;
+import com.EudesGomes.dsmeta.service.SmsService;
 
 @RestControllerAdvice
 @RequestMapping(value = "/sales")
@@ -18,6 +20,8 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
+	@Autowired
+	private SmsService sms;
 	
 	/* metodo busca por todos os id
 	 * @GetMapping
@@ -31,5 +35,11 @@ public class SaleController {
 			@RequestParam(value = "maxDate", defaultValue = "") String maxDate,  
 			Pageable pageable) {
 		return service.findSales(minDate, maxDate, pageable);
+	}
+	
+	//metodo que enviar os sms
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		sms.sendSms(id);
 	}
 }
---
+++ src/main/java/com/EudesGomes/dsmeta/service/SmsService.java
@@ -0,0 +1,52 @@
+package com.EudesGomes.dsmeta.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.EudesGomes.dsmeta.entities.Sale;
+import com.EudesGomes.dsmeta.repositories.SaleRepository;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+@Service
+public class SmsService {
+
+	
+	//@Value busca o valor na vairavel de ambiente porperties, e trás para nossos atributos
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository repository;
+
+	public void sendSms(Long salesId) {
+		
+		Sale sale = repository.findById(salesId).get();
+		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+		
+		String msg = ("Vendedor " + sale.getSellerName() + "foi destaque em " + date
+				+ "com um total de R$ " + String.format("%.2f", sale.getAmount()) );
+		
+		
+
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}

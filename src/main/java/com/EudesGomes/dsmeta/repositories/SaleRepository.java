3e9a0f73e572c49b3bc231a87b47c2a5511384ba
EudesGomes017 <eudesgomes017@gmail.com>
2022-09-10 18:23:48 -0300

    Project created

---
+++ src/main/java/com/EudesGomes/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.EudesGomes.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.EudesGomes.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
---
+++ src/test/java/com/EudesGomes/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EudesGomes.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
